638a2a5bd08244f7c7d151e11e12c038d0a40583
goodmattg <mattgoodman13@gmail.com>
2016-04-23 16:56:08 -0400

    fillCache refactored to take cacheSize as arg.

--- public/helpers/fillCache.js
+++ public/helpers/fillCache.js
@@ -1,20 +1,15 @@
 var getImage = require('./getImage.js');
 
-var fillCache = function () {
+var fillCache = function (cacheSize) {
   return new Promise(function (resolve, reject) {
 
+    var images = [];
+    for (var i = 1; i <= cacheSize; i++) { images.push(String(i)); }
     // Establish promises for each individual image
-    var im1 = getImage('1');
-    var im2 = getImage('2');
-    var im3 = getImage('3');
-    var im4 = getImage('4');
-    var im5 = getImage('5');
-    var im5 = getImage('6');
-    var im5 = getImage('7');
-    var im5 = getImage('8');
+    var imagePromises = images.map(getImage);
 
     // Condition the fill cache promise on fulfill of individual images
-    Promise.all([im1, im2, im3, im4, im5]).then(function (response) {
+    Promise.all(imagePromises).then(function (response) {
       resolve(results);
     }, function (error) {
       reject(error);
